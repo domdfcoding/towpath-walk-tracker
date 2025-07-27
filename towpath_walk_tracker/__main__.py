0d9fdabfc17475c6341c658398a129c744aad044
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-07-27 12:40:25 +0100

    Lint

--- towpath_walk_tracker/__main__.py
+++ towpath_walk_tracker/__main__.py
@@ -29,13 +29,5 @@ Command-line entry point.
 # this package
 from towpath_walk_tracker.flask import app
 
-__all__ = ["hello_world"]
-
-
-@app.route('/')
-def hello_world():
-	return "<p>Hello, World!</p>"
-
-
 if __name__ == "__main__":
 	app.run(debug=True)
