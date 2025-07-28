c5b4411a520090d1bb22f6abb784aef9178144d2
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-07-28 21:04:40 +0100

    Port zoom_state.js to TypeScript

--- src/zoom_state.ts
+++ src/zoom_state.ts
@@ -4,26 +4,31 @@ function updateQueryStringParam (key, value) {
 	// window.history.pushState({}, null, url);
 	window.history.replaceState({}, '', url);
 }
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function setupZoomState (map) {
+function setupZoomState (map: L.Map) {
 	map.on('zoomend', function () {
 		const zoomLvl = map.getZoom();
 		updateQueryStringParam('zoom', zoomLvl);
 	});
+
 	map.on('moveend', function () {
 		const centre = map.getCenter();
 		updateQueryStringParam('lat', centre.lat);
 		updateQueryStringParam('lng', centre.lng);
 	});
 }
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function zoomStateFromURL (defaultZoom, defaultCentre) {
+function zoomStateFromURL (defaultZoom: number, defaultCentre: L.LatLng) {
 	const url = new URL(window.location.href);
+
 	// let zoomLvl = map.getZoom();
 	let zoomLvl = defaultZoom;
 	if (url.searchParams.has('zoom')) {
 		zoomLvl = parseInt(url.searchParams.get('zoom'));
 	}
+
 	// const centre = map.getCenter();
 	const centre = defaultCentre;
 	if (url.searchParams.has('lat')) {
@@ -32,5 +37,6 @@ function zoomStateFromURL (defaultZoom, defaultCentre) {
 	if (url.searchParams.has('lng')) {
 		centre.lng = parseFloat(url.searchParams.get('lng'));
 	}
+
 	return { centre, zoomLvl };
 }
