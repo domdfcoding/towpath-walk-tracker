0f263e7b557df88d9d7df84c45a80e76ce101fb0
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-07-28 13:05:02 +0100

    Refactor walk.js around a class.

--- towpath_walk_tracker/static/walk.js
+++ towpath_walk_tracker/static/walk.js
@@ -4,81 +4,84 @@ let placedMarkerCount = 0;// eslint-disable-line no-unused-vars
 let placedMarkers = [];
 let polyLineWalk = null;// eslint-disable-line no-unused-vars
 
-function clearMarkers () { // eslint-disable-line no-unused-vars
-  for (const m of placedMarkers) m.remove();
-  placedMarkers = [];
-  placedMarkerCount = 0;
-}
+class LeafletWalkPreview {
+  // constructor () {
+  // }
 
-function refreshWalkPreview (propagate = true) { // eslint-disable-line no-unused-vars
-  const placedMarkerLatLng = walkFormGetCoordinates();
-
-  if (propagate) replaceAllPoints(placedMarkerLatLng);
-
-  if (placedMarkerLatLng.length >= 2) {
-    fetch('/get-route', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(placedMarkerLatLng)
-    })
-      .then(res => res.json())
-      .then(coords => {
-        feature_group_current_walk.clearLayers();
-        polyLineWalk = L.polyline(coords, { bubblingMouseEvents: true, color: '#ff0000', dashArray: null, dashOffset: null, fill: false, fillColor: '#ff0000', fillOpacity: 0.2, fillRule: 'evenodd', lineCap: 'round', lineJoin: 'round', noClip: false, opacity: 1.0, smoothFactor: 1.0, stroke: true, weight: 3 }
-        // ).addTo({{this._parent.get_name()}});
-        ).addTo(feature_group_current_walk);
-        console.log('Request complete! response:', coords);
-      });
-  } else {
-    feature_group_current_walk.clearLayers();
+  clearMarkers () {
+    for (const m of placedMarkers) m.remove();
+    placedMarkers = [];
+    placedMarkerCount = 0;
   }
-}
 
-function addMarker (lat, lng) { // eslint-disable-line no-unused-vars
+  refresh (propagate = true) {
+    const placedMarkerLatLng = walkFormGetCoordinates();
+
+    if (propagate) replaceAllPoints(placedMarkerLatLng);
+
+    if (placedMarkerLatLng.length >= 2) {
+      fetch('/get-route', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(placedMarkerLatLng)
+      })
+        .then(res => res.json())
+        .then(coords => {
+          feature_group_current_walk.clearLayers();
+          polyLineWalk = L.polyline(coords, { bubblingMouseEvents: true, color: '#ff0000', dashArray: null, dashOffset: null, fill: false, fillColor: '#ff0000', fillOpacity: 0.2, fillRule: 'evenodd', lineCap: 'round', lineJoin: 'round', noClip: false, opacity: 1.0, smoothFactor: 1.0, stroke: true, weight: 3 }
+          // ).addTo({{this._parent.get_name()}});
+          ).addTo(feature_group_current_walk);
+          console.log('Request complete! response:', coords);
+        });
+    } else {
+      feature_group_current_walk.clearLayers();
+    }
+  }
+
+  addMarker (lat, lng) {
   // Check haven't tried to treat L.latLng as array or array as L.latLng
-  if (lat === undefined) {
-    throw ({ lat: lat });
+    if (lat === undefined) {
+      throw ({ lat: lat });
+    }
+    if (lng === undefined) {
+      throw ({ lng: lng });
+    }
+
+    const marker = L.marker([lat, lng], {});
+
+    placedMarkers.push(marker);
+    placedMarkerCount += 1;
+
+    marker.addTo(feature_group_walk_markers);
+
+    marker.on('contextmenu', e => {
+      removePointWithCoord(e.target.getLatLng());
+      this.removeMarker(e.target);
+      this.refresh();
+    });
   }
-  if (lng === undefined) {
-    throw ({ lng: lng });
+
+  removeMarker (marker) {
+    placedMarkers = placedMarkers.filter(v => v !== marker);
+    placedMarkerCount -= 1;
+    marker.remove();
   }
 
-  const marker = L.marker([lat, lng], {});
-
-  placedMarkers.push(marker);
-  placedMarkerCount += 1;
-
-  marker.addTo(feature_group_walk_markers);
-
-  marker.on('contextmenu', e => {
-    removePointWithCoord(e.target.getLatLng());
-    removeMarker(e.target);
-    refreshWalkPreview();
-  });
-}
-
-function removeMarker (marker) {
-  placedMarkers = placedMarkers.filter(v => v !== marker);
-  placedMarkerCount -= 1;
-  marker.remove();
-}
-
-function snapCoordToLine (lat, lng) { // eslint-disable-line no-unused-vars
+  snapCoordToLine (lat, lng) {
   // Check haven't tried to treat L.latLng as array or array as L.latLng
-  if (lat === undefined) {
-    throw ({ lat: lat });
-  }
-  if (lng === undefined) {
-    throw ({ lng: lng });
+    if (lat === undefined) {
+      throw ({ lat: lat });
+    }
+    if (lng === undefined) {
+      throw ({ lng: lng });
+    }
+
+    const coordinatesArray = geo_json_watercourses.getLayers().map(l => l.feature.geometry.coordinates);
+    const closestLatLng = L.GeometryUtil.closest(map_canal_towpath_walking, coordinatesArray, [lng, lat]);
+    return closestLatLng; // TODO: lat/lng are flipped from the dict labels
   }
 
-  const coordinatesArray = geo_json_watercourses.getLayers().map(l => l.feature.geometry.coordinates);
-  const closestLatLng = L.GeometryUtil.closest(map_canal_towpath_walking, coordinatesArray, [lng, lat]);
-  return closestLatLng; // TODO: lat/lng are flipped from the dict labels
-}
-
-document.querySelector('table.walk-points').addEventListener('changed',
-  (e) => {
+  syncFromForm () {
     const coordinates = walkFormGetCoordinates();
 
     for (const m of placedMarkers) {
@@ -92,7 +95,7 @@ document.querySelector('table.walk-points').addEventListener('changed',
       }
 
       if (!foundMarker) {
-        removeMarker(m);
+        this.removeMarker(m);
       }
     }
 
@@ -108,10 +111,16 @@ document.querySelector('table.walk-points').addEventListener('changed',
       }
 
       if (!foundCoord) {
-        addMarker(c.lat, c.lng);
+        this.addMarker(c.lat, c.lng);
       }
     }
 
-    refreshWalkPreview(false);
+    this.refresh(false);
   }
+}
+
+const walkPreview = new LeafletWalkPreview();
+
+document.querySelector('table.walk-points').addEventListener('changed',
+  (e) => { walkPreview.syncFromForm(); }
 );
